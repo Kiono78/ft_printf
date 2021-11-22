7aefae05a20908f03071849a98ff16f1589da92d
Kiono78 <40069205+Kiono78@users.noreply.github.com>
2021-11-22 16:52:54 +0100

    start count and transform

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bterral <bterral@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 11:42:06 by bterral           #+#    #+#             */
+/*   Updated: 2021/11/22 13:34:50 by bterral          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+
+int	ft_printf(const char *intput, ...);
+
+#endif
