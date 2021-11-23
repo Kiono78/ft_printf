1ef83dc418622915ad12c0375e74119e837fe696
Kiono78 <40069205+Kiono78@users.noreply.github.com>
2021-11-23 18:16:44 +0100

    adding main and removing .c in main directory

--- main.c
+++ main.c
@@ -6,20 +6,25 @@
 /*   By: bterral <bterral@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 11:52:03 by bterral           #+#    #+#             */
-/*   Updated: 2021/11/22 16:06:55 by bterral          ###   ########.fr       */
+/*   Updated: 2021/11/23 18:08:45 by bterral          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft/libft.h"
-#include "ft_printf.h"
+#include "./includes/ft_printf.h"
 
 int main(int argc, char *argv[])
 {
 	char i = '6';
-	// printf("hello %d\n", i);
-	// write(1, &i, 1);
-	printf("ft_printf: %d\n\n", ft_printf("test"));
-	ft_printf("test %q");
+	char *str = "hello";
+	void *test;
+
+	test = malloc(sizeof(void) * 2);
+	int nb = 856;
+	ft_printf("ft_printf: %p %p \n", 0, 98);
+	printf(" %p %p \n", 56, 98);
+
+	// printf("sizeof unsigned long int: %lu", sizeof(void *));
 	return (0);
 }
