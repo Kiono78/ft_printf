3a5e731ac631521fad1c10ba364e0b8848292d4b
Kiono78 <40069205+Kiono78@users.noreply.github.com>
2021-12-05 12:04:09 +0100

    removing stdio

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,13 +6,11 @@
 /*   By: bterral <bterral@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 11:45:24 by bterral           #+#    #+#             */
-/*   Updated: 2021/12/05 11:42:25 by bterral          ###   ########.fr       */
+/*   Updated: 2021/12/05 12:03:41 by bterral          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
-
-#include <stdio.h>
 
 int	handle_variable(char c, va_list	args)
 {
